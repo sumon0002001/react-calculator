3947e4e0a0f380039890d28fd0a2c6a08e788d45
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-04-14 05:04:35 +0600

    Add codes to Display Components

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Display = () => {
+const Display = props => {
+    const {result} = props;
+    return (
+      <div>
+        {result}
+      </div>
+    );
+};
 
+Display.propTypes = {
+    result: PropTypes.string,
 }
 
-
-
-
-
-
+Display.defaultProps = {
+    result: '0'
+}
 
 export default Display;
