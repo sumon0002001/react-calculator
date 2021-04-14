ba43be1da6912b4ba065cca465fb01e8723e9624
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-04-15 00:43:51 +0600

    Add function to the calculate.js file

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -4,8 +4,21 @@ const calculate = (calculator, buttonName) => {
   let { total, next } = calculator;
   const { operation } = calculator;
   if (buttonName === '+/-') {
-      total *= -1;
-      
+    total *= -1;
+    next *= -1;
+    Operate(total, next, operation);
   }
 
-}
+  if (
+      buttonName === '+'
+      || buttonName === '-'
+      || buttonName === '*'
+      || buttonName === '÷'
+  ) {
+      total = Operate(total, next, operation)
+  }
+
+  return calculator;
+};
+
+export default calculate;
