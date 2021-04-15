ee7daaeec92f18e32bae5e62017585c61f4e83e4
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-04-16 03:14:09 +0600

    Add state and class to App

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,33 @@ import ButtonPanel from './ButtonPanel';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <>
-      <header className="App-header">
-        <p>
-          This is the React-Calculator
-        </p>
-      </header>
-      <Display />
-      <ButtonPanel />
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+  };
+}
+  render() {
+    return (
+      <>
+        <header className="App-header">
+          <p>
+            This is the React-Calculator
+          </p>
+        </header>
+        <Display 
+        result = {next || total || '0'}
+        details={}
+        />
+        <ButtonPanel />
+      </>
+    );
+
+  }
+  
 }
 
 export default App;
