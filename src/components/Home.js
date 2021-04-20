5e0ed5428334c98ed6deb386533f8caa96e2df13
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-04-20 12:03:03 +0600

    Add Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,23 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <h3>
+            <h1>
                 Welcome to Our Page
-            </h3>
+            </h1>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Aliquid, facere sapiente. Distinctio facilis dignissimos, 
+            ipsam vero qui asperiores. Sapiente esse cupiditate saepe tempora, 
+            eum totam quibusdam voluptatem quae labore aliquid?
+            </p>
+            <br/>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Aliquid, facere sapiente. Distinctio facilis dignissimos, 
+            ipsam vero qui asperiores. Sapiente esse cupiditate saepe tempora, 
+            eum totam quibusdam voluptatem quae labore aliquid?
+            </p>
+
 
         </div>
     )
